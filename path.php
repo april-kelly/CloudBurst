1bf830a33a12cb2c25fcebccc913419221caff5d
liam-kelly <liam@bluetent.com>
2013-10-24 16:00:23 -0600

    Added path.php.

---
+++ path.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Name:        path.php
+ * Description: Defines the ABSPATH Constant
+ * Date:        10/24/13
+ * Programmer:  Liam Kelly
+ */
+
+define('ABSPATH', dirname(__FILE__).'/');
