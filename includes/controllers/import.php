e974ad16744e949ecab5caa26dfb0302e4899198
liam-kelly <liam@bluetent.com>
2013-10-24 23:01:32 -0600

    Continued work on dirscan section of video import system.

--- includes/controllers/import.php
+++ includes/controllers/import.php
@@ -13,4 +13,33 @@ if(!(defined('ABSPATH'))){
 
 //variable definitions
 $debug = true;
-$location = '';
+$location = '../../../video/';
+
+//Start looking for files to import
+$array = scandir($location);
+
+//Get rid of the . , .. and any files
+$clean = array();
+$i = 0;
+foreach($array as $item){
+
+    //Find files, .. , and .
+    if(!(preg_match('/\./', $item))){
+
+        //Add directories back
+        $clean[$i] = $item;
+        $i++;
+
+    }
+
+}
+
+//Look for Subdirectories
+foreach($clean as $item){
+
+    //Look for files
+    $sub = scandir($location.$item.'/');
+    echo "Subdirectory: $item \r\n";
+    var_dump($sub);
+}
+
