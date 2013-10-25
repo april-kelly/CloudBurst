57dd7f9ba4966dce0c09c5253312a9b5ee1eb7dc
liam-kelly <liam@bluetent.com>
2013-10-24 22:47:50 -0600

    Started work on the video import system.

---
+++ includes/controllers/import.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Name:        Video import tool
+ * Description: Video import tool
+ * Date:        10/24/13
+ * Programmer:  Liam Kelly
+ */
+
+//includes
+if(!(defined('ABSPATH'))){
+    require_once('../../path.php');
+}
+
+//variable definitions
+$debug = true;
+$location = '';
