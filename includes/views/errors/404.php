4de4f1473352c0694c679dd5ba9f5d1d76150dc8
liam-kelly <liam@bluetent.com>
2013-10-26 22:39:15 -0600

    Fixed 404 broken image include.

--- includes/views/errors/404.php
+++ includes/views/errors/404.php
@@ -1,3 +1,3 @@
 <h3>Opps, this page does not exist!</h3>
-<img src="../images/404.jpg" alt="This 404 page now --Picture of Rainbow Dash's head-- 20% cooler" title="Yes, really!"/>
+<img src="./includes/views/images/404.jpg" alt="This 404 page now --Picture of Rainbow Dash's head-- 20% cooler" title="Yes, really!"/>
 <br />Bad "20% cooler" reference, I know.
