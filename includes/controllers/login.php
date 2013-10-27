daebe7902503d30ea6591c0e1913f4042df4c41c
liam-kelly <liam@bluetent.com>
2013-10-26 18:59:11 -0600

    Started work on login system.

---
+++ includes/controllers/login.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Name:        Login system
+ * Description: Logs users in.
+ * Date:        10/26/13
+ * Programmer:  Liam Kelly
+ */
+
+//Includes
+if(!(defined('ABSPATH'))){
+    require_once('../path.php');
+}
+
