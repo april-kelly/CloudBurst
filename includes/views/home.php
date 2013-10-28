f880266c5a856172c354ada6b162118dabde7e2a
Caleb R. Begly <calebbegly@gmail.com>
2013-10-28 07:10:37 -0600

    home page

---
+++ includes/views/home.php
@@ -0,0 +1,19 @@
+<ul class="bxslider">
+    <li class="slide"><img src="http://placehold.it/200x230&text=Show%201"></li>
+    <li class="slide"><img src="http://placehold.it/200x230&text=Show%202"></li>
+    <li class="slide"><img src="http://placehold.it/200x230&text=Show%203"></li>
+    <li class="slide"><img src="http://placehold.it/200x230&text=Show%204"></li>
+    <li class="slide"><img src="http://placehold.it/200x230&text=Show%205"></li>
+    <li class="slide"><img src="http://placehold.it/200x230&text=Show%206"></li>
+    <li class="slide"><img src="http://placehold.it/200x230&text=Show%207"></li>
+    <li class="slide"><img src="http://placehold.it/200x230&text=Show%208"></li>
+    <li class="slide"><img src="http://placehold.it/200x230&text=Show%209"></li>
+</ul>
+<script>
+    $(document).ready(function(){
+        $(".bxslider").bxSlider({
+            slideMargin: 10,
+            slideWidth: 200        
+        });
+    });
+</script>
