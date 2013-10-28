0435dcd611f210d5e2e96ad4ab3c62cfdd3f16c9
Caleb R. Begly <calebbegly@gmail.com>
2013-10-28 11:35:03 -0600

    additional styling

--- includes/views/home.php
+++ includes/views/home.php
@@ -1,3 +1,4 @@
+<h2>New Shows</h2>
 <ul class="bxsliderCar">
     <li class="slide"><img src="http://placehold.it/200x230&text=Show%201"></li>
     <li class="slide"><img src="http://placehold.it/200x230&text=Show%202"></li>
@@ -8,28 +9,16 @@
     <li class="slide"><img src="http://placehold.it/200x230&text=Show%207"></li>
     <li class="slide"><img src="http://placehold.it/200x230&text=Show%208"></li>
     <li class="slide"><img src="http://placehold.it/200x230&text=Show%209"></li>
+    <li class="slide"><img src="http://placehold.it/200x230&text=Show%2010"></li>
+    <li class="slide"><img src="http://placehold.it/200x230&text=Show%2011"></li>
 </ul>
+<h2>Favorites</h2>
 <ul class="bxsliderCar">
     <li class="slide"><img src="http://placehold.it/200x230&text=Show%201"></li>
     <li class="slide"><img src="http://placehold.it/200x230&text=Show%202"></li>
     <li class="slide"><img src="http://placehold.it/200x230&text=Show%203"></li>
     <li class="slide"><img src="http://placehold.it/200x230&text=Show%204"></li>
     <li class="slide"><img src="http://placehold.it/200x230&text=Show%205"></li>
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%206"></li>
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%207"></li>
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%208"></li>
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%209"></li>
-</ul>
-<ul class="bxsliderCar">
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%201"></li>
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%202"></li>
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%203"></li>
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%204"></li>
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%205"></li>
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%206"></li>
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%207"></li>
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%208"></li>
-    <li class="slide"><img src="http://placehold.it/200x230&text=Show%209"></li>
 </ul>
 <script>
     $(document).ready(function(){
@@ -38,8 +27,22 @@
             slideWidth: 200,
             minSlides: 1,
             maxSlides: 10,
-            moveSlides: 1,
+            moveSlides: 3,
             infiniteLoop: false
         });
     });
 </script>
+<style>
+    .bx-wrapper .bx-viewport{
+        background: transparent;
+        border-top: solid #000 3px;
+        border-bottom: solid #000 3px;
+        border-left: none;
+        border-right: none;
+    }
+    
+    .bxsliderCar {
+        padding-top: 0px;
+        margin-top: 0px;
+    }
+</style>
