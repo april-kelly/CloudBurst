4734973752016f1665a46651204afe2d3362aed7
liam-kelly <liam@bluetent.com>
2013-10-31 22:14:07 -0600

    Outlined new video class.

--- includes/models/video.php
+++ includes/models/video.php
@@ -1,7 +1,67 @@
 <?php
 /**
- * Name:        
- * Description: 
+ * Name:        Video.php
+ * Description: Handles video import and serves as an abstraction layer to the database
  * Date:        10/31/13
  * Programmer:  Liam Kelly
  */
+
+class video {
+
+    //read_meta
+    public function read_meta($filename){
+
+        /**
+         * Reads metadata from a video file
+         */
+
+    }
+
+    //lookup
+    public function lookup(){
+
+        /**
+         * Determines if a file is already in the database
+         */
+
+    }
+
+    //insert
+    public function insert(){
+
+        /**
+         * Inserts a video file into the database
+         */
+
+    }
+
+    //fetch_imdb
+    public function fetch_imdb() {
+
+        /**
+         * Fetches meta data from IMDb
+         */
+
+    }
+
+
+    //update_metadata
+    public function update_metadata(){
+
+        /**
+         * Updates the metadata table with the contents of this object
+         */
+
+    }
+
+    //update_media
+    public function update_media(){
+
+        /**
+         * Updates the media table with the contents of this object
+         */
+
+    }
+
+
+}
