1cdd5e1c2b75ea2532e59c52baa7db90f8e7fb66
liam-kelly <liam@bluetent.com>
2013-11-12 11:16:02 -0700

    Still working on episode list.

--- includes/views/list.php
+++ includes/views/list.php
@@ -20,17 +20,20 @@ $episode_list = $video->fetch_episodes($results['metadata'][0]['imdb_id']);
 <b><?php echo $results['metadata'][0]['title']; ?></b>
 <ul>
 <?php
-if(is_array($episode_list)){
 
     $i = 0;
     foreach($episode_list as $episode){
         //var_dump($episode);
 
-        echo '<li>'.$episode[$i]['metadata'][0]['season'].'</li>';
+        echo '<li>';
+        echo '<a href="./?p=video&id='.$episode[$i]['metadata_id'].'">Play</a>';
+        echo $episode[$i]['metadata'][0]['season'].' ';
+        echo $episode[$i]['metadata'][0]['episode'].' ';
+        echo $episode[$i]['metadata'][0]['episode_name'].' ';
+        echo $episode[$i]['metadata'][0]['episode_description'];
+        echo '</li>';
         $i++;
     }
 
-
-}
 ?>
 </ul>
