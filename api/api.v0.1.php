4e5fa722a02b7f09a11422a3d37c2e3f3ff36ca0
liam-kelly <liam@bluetent.com>
2013-11-16 22:04:55 -0700

    Added api request handler and started work on api v0.1

---
+++ api/api.v0.1.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Name:        CloudBurst API Version 0.1
+ * Date:        11/16/13
+ * Programmer:  Liam Kelly
+ */
+
+var_dump(url);
+var_dump(format);
---
+++ api/index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Name:        CloudBurst api request handler
+ * Description: Sends requests to the correct api version
+ * Date:        11/16/13
+ * Programmer:  Liam Kelly
+ */
+
+//Fetch the request
+$url = $_REQUEST['url'];
+
+//Determine the api version and format
+$array = explode('/', $_REQUEST['url']);
+$version = $array[1];
+$format = $array[2];
+
+if(file_exists('api.'.$version.'.php')){
+
+    //The api version exists, invoke it
+    define('url', $url);
+    define('format', $format);
+    require_once('api.'.$version.'.php');
+
+
+}else{
+
+    //The requested api version does not exist
+    header($_SERVER['SERVER_PROTOCOL'].'500 Internal Server Error');
+
+}
