870920fce004488c86fda000ef0b108928e77b6c
Liam <kd0hdf@gmail.com>
2013-12-18 17:48:17 -0700

    More ui changes.

--- includes/views/settings.php
+++ includes/views/settings.php
@@ -1,8 +1,16 @@
-<!--
-<div id="sidebar">
-    <p>Menu</p>
-</div>
--->
 <div id="set">
+
+    <div id="sidebar">
+
+        <?php
+
+            //Include the settings menu
+            require_once(ABSPATH.'includes/views/settings_menus/menu.php')
+
+        ?>
+
+    </div>
+
     <p>This is where settings will go.</p>
+
 </div>
---
+++ includes/views/settings_menus/menu.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="?p=tv">TV</a></li>
+    <li><a href="?p=live">Live TV</a></li>
+    <li><a href="?p=settings">Settings</a></li>
+    <li><a href="?p=logout">Hi, <?php echo $name;?></a></li>
+</ul>
